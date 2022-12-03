5e1b36d4d78da6afdba9706ba4abc746a818a9c3
isaacd <isaacdarzi@gmail.com>
2022-12-03 18:53:24 -0500

    added seed images

--- app/seeds/tweets.py
+++ app/seeds/tweets.py
@@ -4,10 +4,12 @@ def seed_tweets():
     tweet1 = Tweet(
         userId=1,
         body="I'm a tweet"
+        image="https://itweetbucket.s3.amazonaws.com/1480bf70b4ab4dd59b0aca936591cd42.jpeg"
     )
     tweet2 = Tweet(
         userId=1,
         body="I'm another tweet"
+        image="https://itweetbucket.s3.amazonaws.com/8d73861d59d04d1c838db42fd6fb61af.jpg"
     )
     tweet3 = Tweet(
         userId=2,
